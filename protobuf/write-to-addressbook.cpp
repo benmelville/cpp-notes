6a5d1af4718960a20b3bcfb25ef847b263673c69
benlyallmelville <benlyallmelville@gmail.com>
2025-06-19 12:16:02 -0400

    feat: initial write message script

---
+++ protobuf/write-to-addressbook.cpp
@@ -0,0 +1,10 @@
+#include "addressbook.pb.h"
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+    std::cout << "google verify check passed" << std::endl;
+    return 0;
+}
