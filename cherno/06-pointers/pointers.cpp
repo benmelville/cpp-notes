651ed93dbf7bf03f60b9a48f5b3ebfb7f237ba6e
benlyallmelville <benlyallmelville@gmail.com>
2026-03-06 16:58:17 -0500

    feat: add some more notes to pointers

--- cherno/06-pointers/pointers.cpp
+++ cherno/06-pointers/pointers.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <cstring>
 #define LOG(x) std::cout << x << std::endl;
 
 int main()
@@ -9,13 +9,21 @@ int main()
   void* ptr1 = NULL;
   void* ptr2 = nullptr;
 
-  int var = 8;
+  long long var = 8;
   LOG(var);
   
-  int* varptr = &var; // if we changed this to void* varptr = &var; we would get the same result
+  long long* varptr = &var; // if we changed this to void* varptr = &var; we would get the same result
   // dereference the pointer - we need to tell the compiler what type this is so it knows how many bytes to write
   // otherwise, it has no way of knowing
   *varptr = 10;
   LOG(var);
+  int* test = new int(7);
+  LOG(test);
+  char* buffer = new char[9]; // allocates 8 bytes and returns a pointer to the beginning of that block of memory
+  std::memset(buffer, 10, 9);
 
+  char** buffer_pointer = &buffer;
+
+
+  delete[] buffer;
 }
